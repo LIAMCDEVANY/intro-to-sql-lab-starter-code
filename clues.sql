2276960fa99ee9c1ac2c5bafb35939d6864010a7
Liam Devany <YOUR_EMAIL>
2026-05-07 15:29:06 +0100

    Complete intro to SQL lab

--- clues.sql
+++ clues.sql
@@ -1,50 +1,53 @@
 -- Clue #1:
 SELECT name, code, population
-FROM country
+FROM countries
 WHERE region = 'Southern Europe'
 ORDER BY population ASC
 LIMIT 1;
 
 -- Clue #2:
 SELECT language
-FROM countrylanguage
+FROM countrylanguages
 WHERE countrycode = 'VAT'
-  AND isofficial = 'T';
+  AND isofficial = true;
 
 -- Clue #3:
 SELECT c.name, c.code
-FROM country c
-JOIN countrylanguage cl
+FROM countries c
+JOIN countrylanguages cl
   ON c.code = cl.countrycode
 WHERE cl.language = 'Italian'
-  AND cl.isofficial = 'T'
+  AND cl.isofficial = true
   AND cl.percentage = 100
   AND c.code <> 'VAT';
 
 -- Clue #4:
 SELECT name
-FROM city
+FROM cities
 WHERE countrycode = 'SMR'
   AND name <> 'San Marino';
 
 -- Clue #5:
 SELECT ci.name, co.name AS country
-FROM city ci
-JOIN country co
+FROM cities ci
+JOIN countries co
   ON ci.countrycode = co.code
 WHERE ci.name LIKE 'Serra%'
   AND co.continent = 'South America';
 
 -- Clue #6:
 SELECT name
-FROM city
+FROM cities
 WHERE id = (
   SELECT capital
-  FROM country
+  FROM countries
   WHERE code = 'BRA'
 );
 
 -- Clue #7:
 SELECT name, countrycode
-FROM city
-WHERE population BETWEEN 91000 AND 92000;
+FROM cities
+WHERE population BETWEEN 91000 AND 92000
+  AND countrycode = 'BRA';
+
+  Vatican City → Italian → San Marino → Serravalle → Serra, Brazil → Brasília → one of four Brazilian cities
